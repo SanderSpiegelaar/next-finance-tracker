f33efe1f806da9b32540f51e1258dc9bf7ccc648
Sander Spiegelaar <sanderspiegelaar@gmail.com>
2024-06-27 10:21:53 +0200

    Create account form

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,9 +1,37 @@
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account"
+import { z } from "zod"
 
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet"
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account"
+import { AccountForm } from "@/features/accounts/components/account-form"
+import { useCreateAccount } from "@/features/accounts/api/use-create-account"
+
+import { insertAccountSchema } from "@/db/schema"
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle
+} from "@/components/ui/sheet"
+
+const formSchema = insertAccountSchema.pick({
+	name: true
+})
+
+type FormValues = z.input<typeof formSchema>
 
 export const NewAccountSheet = () => {
 	const { isOpen, onClose } = useNewAccount()
+
+	const mutation = useCreateAccount()
+
+	const onSubmit = (values: FormValues) => {
+		mutation.mutate(values, {
+			onSuccess: () => {
+				onClose()
+			}
+		})
+	}
+
 	return (
 		<Sheet
 			open={isOpen}
@@ -12,8 +40,14 @@ export const NewAccountSheet = () => {
 			<SheetContent className="space-y-4">
 				<SheetHeader>
 					<SheetTitle>New Account</SheetTitle>
-					<SheetDescription>Create a new account to track your transactions.</SheetDescription>
+					<SheetDescription>
+						Create a new account to track your transactions.
+					</SheetDescription>
 				</SheetHeader>
+				<AccountForm
+					onSubmit={onSubmit}
+					disabled={mutation.isPending}
+				/>
 			</SheetContent>
 		</Sheet>
 	)
